f034dcb00a038ae33a9297f28cce9432b7ad24d0
Mintu Sikder <mintusikder15@gmail.com>
2026-06-23 22:15:40 +0600

    added basic server setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+const PORT = process.env.PORT || 3000;
+
+async function server() {
+  try {
+    await prisma.$disconnect();
+  } catch (e) {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+  app.listen(PORT, () => {
+    console.log(`Server is running ${PORT}`);
+  });
+}
+server();
